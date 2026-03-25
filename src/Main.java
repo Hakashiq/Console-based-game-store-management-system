7c6a3a35ba8320348f5fe169a9593e128d01d4c6
Hakashiq <145635637+Hakashiq@users.noreply.github.com>
2026-03-25 20:53:06 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import app.GameShopApp;
-
 public class Main {
     public static void main(String[] args) {
+        // Run main and it will launch the console
         new GameShopApp().start();
     }
 }
